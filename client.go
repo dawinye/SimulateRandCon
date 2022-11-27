0003ad2bb6d782d548e94bbaf0da91f78ddcdce1
yeindaw <yedawin@gmail.com>
2022-11-27 15:06:12 -0500

    more details on client.go

--- client.go
+++ client.go
@@ -5,12 +5,48 @@ import (
 	"sync"
 	"strconv"
 )
+type Map struct {
+	syncMap sync.Map
+}
+func (m *Map) Load(i int) chan interface{} {
+	val, ok := m.syncMap.Load(i)
+	if ok {
+		return val.(chan interface{})
+	} else {
+		return nil
+	}
+}
+func (m *Map) Store(i int, value chan interface{}) {
+	m.syncMap.Store(i, value)
+}
+func (m *Map) Exists(i int) bool {
+	_, ok := m.syncMap.Load(i)
+	return ok
+}
+func (m *Map) Delete(i int) {
+	m.syncMap.Delete(i)
+}
+var nodes Map
+type channel chan interface{float64}
 
-func createNode(i int, val float64, m sync.Map) {
 
-	//m.Store(i, )
+func createNode(i int, val float64, m Map, waitsFor int) {
+	dataChannel := make(channel, waitsFor)
+	m.Store(i, dataChannel)
 	fmt.Printf("%d, %f\n", i, val)
 }
+
+
+func findConcensus(i int, m Map, N int){
+	for j:= 0; j < N; {
+		channel := m.Load(j)
+		if channel == nil{
+			fmt.Println("channel loaded incorrectly")
+		}
+		channel <- i
+	}
+}
+//TODO
 //func simulateDelay()
 func main() {
 	//TODO: ERROR CHECKING
@@ -21,19 +57,33 @@ func main() {
 	args := os.Args
 	//serverNo := args[1]
 
-	N := args[2]
-	f := args[3]
+	N, err:= strconv.Atoi(args[2])
+	if err != nil {
+		fmt.Println("The second value that you supplied was not an integer, please rerun the program")
+	}
+	f, err:= strconv.Atoi(args[3])
+	if err != nil {
+		fmt.Println("The third value that you supplied was not an integer, please rerun the program")
+	}
+
 	values := args[4:]
-	nodes := sync.Map{}
+	if len(values) != N{
+		fmt.Println("The length of the value list is not the same as the input N, please rerun the program. ")
+	}
+	
 
 	for i, val := range values {
 		value, err := strconv.ParseFloat(val,32)
 		//fmt.Println(val)
 		if err != nil{
-			fmt.Printf("The %dth value that you supplied was not a float, please rerun the program", i+1)
+			fmt.Printf("The %dth value that you supplied was not a float, please rerun the program\n", i+1)
 			
 		}
-		createNode(i, value, nodes)
+
+		createNode(i, value, nodes, N-f)
+	}
+	for i, _ := range values {
+		go findConcensus(i)
 	}
 	fmt.Println(N,f,values)
 }
